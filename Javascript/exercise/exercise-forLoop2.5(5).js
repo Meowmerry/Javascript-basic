bcf6bacfc04638118eb53e0211b65a1b2f639e6a
meowmerry <meow3525@gmail.com>
2020-02-18 20:25:48 +0700

    [added] exercise Javascript

---
+++ Javascript/exercise/exercise-forLoop2.5(5).js
@@ -0,0 +1,13 @@
+
+//  2.5. แบบฝึกหัด
+//  (5.)	5.	เปลี่ยน code for loop ด้านล่างนี้ให้เป็น while loop โดยที่ผลลัพธ์ยังเหมือนเดิม
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+
+let i = 0;
+while( i < 3 ){
+  alert( `number ${i}!` );
+  i++;
+}
